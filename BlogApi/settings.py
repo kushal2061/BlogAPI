4b9411c3a7ed3c5cb818c125145c6c1e147a4d0c
Kushal Kandel <136981558+kushal2061@users.noreply.github.com>
2026-07-06 19:58:01 +0545

    production ready

--- BlogApi/settings.py
+++ BlogApi/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$btfuy5)oef1lszn9k7ph%eom3h2&j_b+*rjacu&-7l-+avytd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",
+    "127.0.0.1",
+    ".onrender.com",]
 
 
 # Application definition
@@ -128,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
