69e3b0548acf107d7b44c8fc7e5b8ce168c37335
Kushal Kandel <136981558+kushal2061@users.noreply.github.com>
2026-08-08 19:59:21 +0545

    Create Django superuser

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py createsuperuser --noinput --username admin --email ""
+python manage.py createsuperuser --noinput
