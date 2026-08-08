61fb8149a7f095439411c4bbdc62a1aaa7f5402e
Kushal Kandel <136981558+kushal2061@users.noreply.github.com>
2026-08-08 19:41:53 +0545

    Configure PostgreSQL database

--- BlogApi/settings.py
+++ BlogApi/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,10 +91,9 @@ WSGI_APPLICATION = 'BlogApi.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
